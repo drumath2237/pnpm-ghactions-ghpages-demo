ac3066f8196532717567c13cd0eb4cceaaee4682
drumath2237 <2237k.drumath@gmail.com>
2023-12-16 17:38:54 +0900

    :wrench: vite configのgh-pages用の設定

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-})
+export default defineConfig(({ mode }) => {
+  if (mode === "production") {
+    return {
+      base: "/pnpm-ghactions-ghpages-demo/",
+      plugins: [vue()],
+    };
+  }
+  return {
+    plugins: [vue()],
+  };
+});
